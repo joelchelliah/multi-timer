eedf8febdaa1a0f1e87a040432beab4a7ddc0127
Joel Chelliah <joel.chelliah@bekk.no>
2016-07-05 20:29:05 +0200

    Prevent display sleep while timer window is up

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 'use strict'
-const electron = require('electron')
+const {app, Tray, Menu, powerSaveBlocker, BrowserWindow} = require('electron');
+const path = require('path');
 
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+// const app = electron.app
+// const BrowserWindow = electron.BrowserWindow
 
-let mainWindow
+let appIcon;
+let mainWindow;
+let blocker_id;
+const iconPath = path.join(__dirname, 'images', 'clock.png');
 
-app.on('ready', createWindow)
+app.on('ready', initTray)
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768
-  })
-
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 1024, height: 768});
   mainWindow.loadURL(`file://${ __dirname }/index.html`)
-  mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', function () { mainWindow = null })
+  //mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', function () {
+    if (blocker_id) powerSaveBlocker.stop(blocker_id);
+    mainWindow = null;
+  })
 }
+
+function initTray () {
+  blocker_id = null;
+  appIcon = new Tray(iconPath);
+  createWindow();
+
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Multi-timer',
+      icon: iconPath,
+      click: function() {
+        if(!mainWindow) createWindow();
+        if (blocker_id) powerSaveBlocker.stop(blocker_id);
+        blocker_id = powerSaveBlocker.start('prevent-display-sleep');
+      }
+    },
+    { label: 'Quit',
+      accelerator: 'Command+Q',
+      selector: 'terminate:',
+      click: function() {
+        if (blocker_id) powerSaveBlocker.stop(blocker_id);
+        if (mainWindow) mainWindow.hide();
+        mainWindow = null;
+      }
+    }
+  ]);
+
+  appIcon.setContextMenu(contextMenu);
+}
+
 
 /* Mac Specific things */
 
